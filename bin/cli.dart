1f421d672dfd7c3ee8f5ea8a844136f2203856bb
Ian-Mutuge <99174650+Ian-Mutuge@users.noreply.github.com>
2026-07-20 16:50:12 +0300

    learning dart libraries

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,54 +1,12 @@
 import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:command_runner/command_runner.dart';
 
 const version = '0.0.1';
-void main(List<String> arguments) {
-  if (arguments.isEmpty || arguments.first == 'help') {
-    printUsage();
-  } else if (arguments.first == 'version') {
-    print('Dartpedia CLI version $version');
-  } else if (arguments.first == 'wikipedia') {
-    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
-    searchWikipedia(inputArgs);
-  } else {
-    printUsage();
-  }
+void main(List<String> arguments) async{
+var runner = CommandRunner();
+await runner.run(arguments);
 }
 
-void searchWikipedia(List<String>? arguments) async {
-  final String articleTitle;
-  if (arguments == null || arguments.isEmpty) {
-    print('Please provide an article.');
-    final inputFromStdIn = stdin.readLineSync();
-    if (inputFromStdIn == null || inputFromStdIn.isEmpty) {
-      print('No article title provided. Exiting.');
-      return;
-    }
-    articleTitle = inputFromStdIn;
-  } else {
-    articleTitle = arguments.join(' ');
-  }
-  print('Looking up articles about "$articleTitle". Please wait.');
-  // Calling the API
-  var articleContent = await getWikipediaArticle(articleTitle);
-  print(articleContent);
-}
 
-void printUsage() {
-  print(
-    "The following commands are valid:'help', 'version', 'search<ARTICLE-TITLE>'",
-  );
-}
 
-Future<String> getWikipediaArticle(String articleTitle) async {
-  final url = Uri.https(
-    'en.wikipedia.org', // Wikipedia API domain
-    '/api/rest_v1/page/summary/$articleTitle',
-  );
-  final response = await http.get(url); //http request
-  if (response.statusCode == 200) {
-    return response.body;
-  }
-
-  return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
-}
---
+++ command_runner/lib/src/command_runner_base.dart
@@ -0,0 +1,6 @@
+class CommandRunner {
+ // Runs command-line app logic
+Future<void> run(List<String> input) async {
+ print('CommandRunner received arguments:$input');
+}
+}
